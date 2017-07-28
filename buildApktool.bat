1a1bdcd1e5dd212bcb76a1cee76c393b44817866
spannaa <spannaa@users.noreply.github.com>
2017-07-28 14:37:20 +0100

    Update buildApktool.bat

--- buildApktool.bat
+++ buildApktool.bat
@@ -1,28 +1,16 @@
 @echo off
-mode con:cols=125 lines=40
-if exist "Apktool\" rd /q /s "Apktool"
+mode con:cols=124 lines=30
 git clone git://github.com/iBotPeaches/Apktool.git
 cd Apktool
-echo.
-echo Building apktool...
 call gradlew.bat build shadowJar
-echo.
-cd "%~dp0"
-echo.
-echo Cleaning up...
-copy "Apktool\brut.apktool\apktool-cli\build\libs\apktool-cli-all.jar" "apktool-cli-all.jar" > nul
-if not exist "Extract\" mkdir "Extract"
-cd tools
-7za x -o"..\Extract" "..\apktool-cli-all.jar" > nul
-cd "%~dp0"
-copy "Extract\properties\apktool.properties" "apktool.properties" > nul
-if exist "Extract\" rd /q /s "Extract"
+copy "%~dp0\tools\7za.exe" "7za.exe" > nul
+7za e "brut.apktool\apktool-cli\build\libs\apktool-cli-all.jar" "apktool.properties" -r > nul
 for /f "tokens=*" %%a in ('find /i "application.version="^<apktool.properties') do set %%a
-ren "apktool-cli-all.jar" "apktool_%application.version%.jar"
-if exist "apktool.properties" del /f "apktool.properties"
-if exist "Apktool\" rd /q /s "Apktool"
+copy "brut.apktool\apktool-cli\build\libs\apktool-cli-all.jar" "%~dp0\apktool_%application.version%.jar" > nul
+cd %~dp0
+rd /q /s "Apktool"
 echo.
-echo apktool_%application.version%.jar complete
+echo apktool_%application.version%.jar built sucessfully
 echo.
 pause
 exit
